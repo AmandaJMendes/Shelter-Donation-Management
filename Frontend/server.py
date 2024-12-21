034f844d1c59f5f8d442e17c2123877b256b4192
João Mello <joao.mellof@furg.br>
2024-12-21 19:11:05 -0300

    fix: format frontend server

--- Frontend/server.py
+++ Frontend/server.py
@@ -1,15 +1,19 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='.')
+app = Flask(__name__, static_folder=".")
+
 
 # Serve static files from the "Frontend" directory
-@app.route('/')
-@app.route('/<path:path>')
-def serve(path=''):
-    if path and '.' in path:
+@app.route("/")
+@app.route("/<path:path>")
+def serve(path=""):
+    if path and "." in path:
         print(path)
         return send_from_directory(app.static_folder, path)
-    return send_from_directory(app.static_folder, 'home.html') #Alterei para a 'home', pois entendi que os users terão acesso a ela mesmo não logados. TODO: cria botão de login 
+    return send_from_directory(
+        app.static_folder, "home.html"
+    )  # Alterei para a 'home', pois entendi que os users terão acesso a ela mesmo não logados. TODO: cria botão de login
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=3000)
